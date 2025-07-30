415459f996d58214aae544d7af6984659d6da18c
FERDOUS FERDOUS <you@example.com>
2025-07-30 09:08:51 +0600

    mongoose full setup

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,7 +2,7 @@ import express, { Application, Request, Response } from "express";
 const app: Application = express();
 
 app.get('/', (req : Request, res : Response ) =>{
-  console.log("  console.log(Welcome to Note apps")
+res.send("Welcome to note apps")
 })
 
 
--- src/app/server.ts
+++ src/app/server.ts
@@ -1,9 +1,14 @@
 import { Server } from "http";
 import app from "./app";
+import mongoose from "mongoose";
 let server: Server;
 const PORT =5000
 async function main() {
   try {
+    await mongoose.connect("mongodb+srv://note-app:bZZwZtD8UMSNcLOs@cluster0.frskr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+    console.log("Connecting to Mongodb to using mongoose")
+
     server = app.listen(PORT, () =>{
         console.log(`app is listen on port ${ PORT}`)
     })
