060d1e7d9f6ac453a7a187d68417f22668afd2f4
FERDOUS FERDOUS <you@example.com>
2025-07-30 10:52:05 +0600

    mongoose schema

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,49 @@
 import express, { Application, Request, Response } from "express";
+import { model, Schema } from "mongoose";
 const app: Application = express();
 
-app.get('/', (req : Request, res : Response ) =>{
-res.send("Welcome to note apps")
-})
+// mongoose schema make
+const notSchema = new Schema({
+  title: { type: String, require: true, trim: true },
+  conten: { type: String, default: "" },
+  category: {
+    type: String,
+    enum: ["personal", "work", "study", "others"],
+    default: "personal",
+  },
+  pinned: {
+    type: Boolean,
+    default: false,
+  },
+  tags: {
+    label: { type: String, require: true,},
+    color : {type : String, default : "Gray"}
+  },
+});
 
+const Note = model("Note", notSchema);
+
+// post route
+app.post("/create-not-app", async (req: Request, res: Response) => {
+  const myNote = new Note({
+    title: "Learning express",
+    tags : {
+      label : "Database"
+    }
+  });
+
+  //save file path
+  await myNote.save();
+
+  res.status(201).json({
+    success: true,
+    message: "Note created Successfully",
+    note: myNote,
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to note app");
+});
 
 export default app;
--- src/app/server.ts
+++ src/app/server.ts
@@ -2,19 +2,21 @@ import { Server } from "http";
 import app from "./app";
 import mongoose from "mongoose";
 let server: Server;
-const PORT =5000
+const PORT = 5000;
 async function main() {
   try {
-    await mongoose.connect("mongodb+srv://note-app:bZZwZtD8UMSNcLOs@cluster0.frskr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    await mongoose.connect(
+      "mongodb+srv://note-app:bZZwZtD8UMSNcLOs@cluster0.frskr.mongodb.net/advance-note-app?retryWrites=true&w=majority&appName=Cluster0"
+    );
 
-    console.log("Connecting to Mongodb to using mongoose")
+    console.log("Connecting to Mongodb to using mongoose");
 
-    server = app.listen(PORT, () =>{
-        console.log(`app is listen on port ${ PORT}`)
-    })
+    server = app.listen(PORT, () => {
+      console.log(`app is listen on port ${PORT}`);
+    });
   } catch (error) {
     console.log(error);
   }
 }
 
-main()
+main();
